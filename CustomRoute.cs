b616d2c0b6484be1ba930fbecb0253d2a12cf428
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2019-08-08 09:55:00 -0700

    Initial Check In

---
+++ CustomRoute.cs
@@ -0,0 +1,52 @@
+﻿using Penguin.Persistence.Abstractions.Attributes.Control;
+using System;
+using System.Collections.Generic;
+
+namespace Penguin.Cms.Attributes
+{
+    /// <summary>
+    /// For Dynamic Rendering, allows a property to be tagged with a specific MVC route for rendering
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public sealed class CustomRouteAttribute : Attribute
+    {
+        #region Properties
+
+        /// <summary>
+        /// The DisplayContext that this route applies to
+        /// </summary>
+        public DisplayContext Context { get; set; }
+
+        /// <summary>
+        /// The object dictionary representing the passed in route values
+        /// </summary>
+        public Dictionary<string, object> RouteValues { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs a new instance of this attribute
+        /// </summary>
+        /// <param name="context">The display context this attribute should apply to</param>
+        /// <param name="controller">The name of the controller to route to</param>
+        /// <param name="action">The controller action to route to</param>
+        /// <param name="area">The area the controller resides in</param>
+        /// <param name="namespace">The namespace the controller resides in</param>
+        public CustomRouteAttribute(DisplayContext context, string controller, string action, string area = "", string @namespace = "")
+        {
+            this.RouteValues = new Dictionary<string, object>
+            {
+                { "controller", controller },
+                { "action", action },
+                { "area", area },
+                { "namespace", @namespace }
+            };
+
+            this.Context = context;
+        }
+
+        #endregion Constructors
+    }
+}
---
+++ DisplayType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Penguin.Cms.Attributes
+{
+    /// <summary>
+    /// Specifies that during dynamic display, this property should be treated as a different type
+    /// </summary>
+    public class DisplayType : Attribute
+    {
+        #region Properties
+
+        /// <summary>
+        /// The FullName of the type to treat this property as
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion Properties
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a new instance of this attribute
+        /// </summary>
+        /// <param name="name">The full name of the type to render this property as</param>
+        public DisplayType(string name)
+        {
+            Name = name;
+        }
+
+        #endregion Constructors
+    }
+}
